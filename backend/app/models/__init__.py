d177c650a0967843ffe0e916e1989ee63adea444
ducky <gillianthorne1353@gmail.com>
2026-07-06 18:46:03 -0400

    create init

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.board import Board
+from app.models.recurring_template import RecurringTemplate
+from app.models.stage import Stage
+from app.models.tag import Tag
+from app.models.task import Task, task_tags

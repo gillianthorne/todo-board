29df72a39c143b559a98f086e70499829861e623
ducky <gillianthorne1353@gmail.com>
2026-07-06 18:31:51 -0400

    alembic

---
+++ backend/alembic/versions/4db7dde5c189_baseline.py
@@ -0,0 +1,26 @@
+"""baseline
+
+Revision ID: 4db7dde5c189
+Revises: 
+Create Date: 2026-07-06 18:13:29.801850
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+# revision identifiers, used by Alembic.
+revision: str = '4db7dde5c189'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass

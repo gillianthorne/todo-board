9acd6ba5d24d1aff5aec2ef6b90348257c9f3046
ducky <gillianthorne1353@gmail.com>
2026-07-06 12:40:33 -0400

    login auth

---
+++ app/routers/auth.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, HTTPException, Request
+
+from app.auth import check_password
+from app.schemas.auth import LoginRequest
+
+router = APIRouter()
+
+@router.post("/login")
+def login(request: Request, credentials: LoginRequest):
+    correct_password = check_password(credentials.password)
+
+    if correct_password:
+        request.session["logged_in"] = True
+        return {"message": "Logged in successfully"}
+    else:
+        raise HTTPException(status_code=401, detail="Incorrect password.")
+
+

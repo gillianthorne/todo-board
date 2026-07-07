bf848819587648ac81220f069807c995582869f0
ducky <gillianthorne1353@gmail.com>
2026-07-07 14:41:22 -0400

    add boards to routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,10 +3,11 @@ from decouple import config
 from fastapi import APIRouter, Depends, FastAPI
 
 from app.deps import require_auth
-from app.routers import auth
+from app.routers import auth, board
 
 app = FastAPI()
 
 app.add_middleware(SessionMiddleware, secret_key=config('SECRET_KEY'))
 
 app.include_router(auth.router)
+app.include_router(board.router)

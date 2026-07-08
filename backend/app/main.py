0c87a56900e67abb3e1f125297603b5edfe17e82
ducky <gillianthorne1353@gmail.com>
2026-07-08 15:35:04 -0400

    messing around with this to force it to work

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,31 @@
 from starlette.middleware.sessions import SessionMiddleware
 from decouple import config
-from fastapi import APIRouter, Depends, FastAPI
+from fastapi import APIRouter, Depends, FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.deps import require_auth
 from app.routers import auth, board, stage
 
+# Check if you are in production or local development
+IS_PRODUCTION = config('ENVIRONMENT', default='development') == 'production'
+
 app = FastAPI()
 
-app.add_middleware(SessionMiddleware, secret_key=config('SECRET_KEY'))
+app.add_middleware(
+    SessionMiddleware, 
+    secret_key=config('SECRET_KEY'), 
+    # Use "lax" for local HTTP, "none" for cross-domain production HTTPS
+    same_site="none" if IS_PRODUCTION else "lax", 
+    # Require HTTPS/Secure flag only in production
+    https_only=IS_PRODUCTION 
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[config('FRONTEND_ORIGIN')],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(board.router)
